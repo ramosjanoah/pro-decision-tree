bc5de79a521840e72bf80807d6bf9887d8512ecc
ramosjanoah <ramosj.noah@gmail.com>
2017-09-19 11:40:59 +0700

    init

---
+++ DecisionTree/src/decisiontree/WekaInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decisiontree;
+
+/**
+ *
+ * @author ramosjanoah
+ */
+public class WekaInterface {
+    public static void test(){
+        System.out.println("test");
+    }
+    
+}
