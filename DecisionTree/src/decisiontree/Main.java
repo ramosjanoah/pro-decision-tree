e4c47a72e5d8ee9deb15808eb732cf4533596491
Jovian Christianto <13514101@std.stei.itb.ac.id>
2017-09-30 11:54:48 +0700

    draft myID3

--- DecisionTree/src/decisiontree/Main.java
+++ DecisionTree/src/decisiontree/Main.java
@@ -23,19 +23,31 @@ public class Main {
      */
     public static void main(String[] args) throws IOException, Exception {
         // TODO code application logic here
+        
+        Evaluation eval;
 
         Instances data = WekaInterface.loadDataset("contact-lenses.arff");
 //        System.out.println(data);
 //        System.out.println("");
 
-//        Id3 SimpleId3 = WekaInterface.createAndTrainId3(data);        
-//        System.out.println(SimpleId3.toString());
+        Id3 SimpleId3 = WekaInterface.createAndTrainId3(data);        
 
-        J48 SimpleJ48 = WekaInterface.createAndTrainJ48(data);
-        System.out.println(SimpleJ48.toString());
+//        J48 SimpleJ48 = WekaInterface.createAndTrainJ48(data);
+//        System.out.println(SimpleJ48.toString());
         
-        Evaluation eval = WekaInterface.evaluateModelWithInstances(SimpleJ48, data);
-        System.out.println(eval.toMatrixString());
+        myID3 tree = new myID3();
+        tree.buildClassifier(data);
+        eval = WekaInterface.evaluateModelWithInstances(tree, data);
+        System.out.println(eval.toSummaryString());
+        eval = WekaInterface.evaluateModelWithInstances(SimpleId3, data);
+        System.out.println(eval.toSummaryString());        
+
+
+
+        System.out.println(tree.toString());
+        System.out.println(SimpleId3.toString());
+
+        
         
     }
     
